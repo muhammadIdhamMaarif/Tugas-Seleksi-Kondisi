38f1ae227c2aef9277a179830b77ba6dbb8bc0d6
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-09-30 12:38:20 +0700

    Create Menentukan Kegenapan Angka.cpp

---
+++ Menentukan Kegenapan Angka.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2 == 0) cout << "Genap" << endl;
+    else cout << "Ganjil" << endl;
+    return 0;
+}
